4821bba057db0f2799eff631197e5608cb1b0243
Andrey Svyrskyi <akkad1v@gmail.com>
2024-02-01 03:31:07 +0200

    'update'

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+// import Cssloader from 'css-loader/dist/';
