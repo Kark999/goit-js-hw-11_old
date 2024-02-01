4805c632bceb82ccefeba808d6b9e1da3e7a0292
Andrey Svyrskyi <akkad1v@gmail.com>
2024-02-02 01:52:14 +0200

    aaa

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // import Cssloader from 'css-loader/dist/';
+
+function getImagesByType(q) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const PARAMS = `?q=${q}&image_type='photo'&orientation='horizontal'&safesearch=true`;
+  const url = BASE_URL + PARAMS;
+
+  const options = {
+    key: '',
+  };
+
+  return fetch(url);
+}
+getImagesByType(dog);
