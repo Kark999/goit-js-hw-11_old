9fde5bc09afd83969958058bd7821ffca96612b8
Andrey Svyrskyi <akkad1v@gmail.com>
2024-02-23 06:02:20 +0200

    'update'

--- src/main.js
+++ src/main.js
@@ -6,47 +6,54 @@ register();
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
+    img: './img/save-the-children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
+    img: './img/project-hope.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
+    img: './img/int-med-corps.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: null,
+    img: './img/razom.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
+    img: './img/action-against-hunger.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: null,
+    img: './img/prytula.png',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: null,
+    img: './img/msf.png',
   },
 
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
+    img: './img/world-vision.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: null,
+    img: './img/united24.png',
   },
 ];
+
+const swiper = new Swiper('.swiper', {
+  width: 159,
+  height: 292,
+});
+
+function name(params) {}
